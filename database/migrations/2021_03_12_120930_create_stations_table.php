32b5d0786d1b8e8ed6182b8382270db98cc2a1c4
Ibrahim hamdy <mslm356@outlook.com>
2021-03-12 14:50:28 +0200

    Update the name of the station table to be unique.

--- database/migrations/2021_03_12_120930_create_stations_table.php
+++ database/migrations/2021_03_12_120930_create_stations_table.php
@@ -15,7 +15,7 @@ class CreateStationsTable extends Migration
     {
         Schema::create('stations', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }
