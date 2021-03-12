61320ab6d8f719de5be5b70676a6f737af9d270d
Ibrahim hamdy <mslm356@outlook.com>
2021-03-12 15:30:19 +0200

    Add the trips table migration.

---
+++ database/migrations/2021_03_12_132034_create_trips_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('start_date_and_time');
+            $table->timestamp('end_date_and_time');
+
+            $table->foreignId('bus_id');
+            $table->foreign('bus_id')->references('id')->on('buses');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
